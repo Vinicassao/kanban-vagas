759d4b5798fb1dc185f851d30f74924706d96193
Vinicassao <viniciusconta1@outlook.com.br>
2026-07-01 21:23:52 -0300

    feat: implementa servidor Express com configuração inicial e rotas

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express()
+
+app.use(express.json());
+
+app.listen(3333, () => {
+    console.log('Servidor iniciado na porta 3333');
+});
+
+app.get("/", (req, res) => {
+    res.send("Servidor funcionando")
+});
