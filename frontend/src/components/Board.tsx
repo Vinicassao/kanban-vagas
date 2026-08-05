a371ab6ec755e0f8ccf6195dfccbef36cd024299
Vinicassao <viniciusconta1@outlook.com.br>
2026-08-04 22:08:55 -0300

    feat: aprimorando o estilo dos componentes do board, column e card

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -2,7 +2,7 @@ import { Column } from "./Column";
 
 export function Board() {
   return (
-    <div className="flex gap-6 p-8">
+    <div className="flex gap-6 p-8 overflow-x-auto min-h-screen bg-white">
       <Column title="Aplicado"/>
       <Column title="Teste"/>
       <Column title="Entrevista"/>
